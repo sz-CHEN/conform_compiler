c0b69be48f84ac8d4536c085f6d00133d069b978
chen_shizeng <cszseki@qq.com>
2023-03-29 23:11:45 +0800

    友好的预处理器缩进

--- uniform_compiler.h
+++ uniform_compiler.h
@@ -13,10 +13,10 @@
 #define UNIFORM_COMPILER_H
 
 #ifdef __GNUC__
-#pragma push_macro("GCC_VERSION")
-#undef GCC_VERSION
-#define GCC_VERSION                                                            \
-    (__GNUC__ * 10000 + __GNUC_MINOR__ * 100 + __GNUC_PATCHLEVEL__)
+#    pragma push_macro("GCC_VERSION")
+#    undef GCC_VERSION
+#    define GCC_VERSION                                                        \
+        (__GNUC__ * 10000 + __GNUC_MINOR__ * 100 + __GNUC_PATCHLEVEL__)
 /* Test for GCC > 3.2.0 */
 /* #if GCC_VERSION > 30200 */
 /* #endif */
@@ -25,7 +25,7 @@
 #define UNIFORM_COMPILER_CONCAT_AS_STR(a, b) #a #b
 
 #if !defined(__STDC_VERSION__)
-#define __STDC_VERSION__ 198912L
+#    define __STDC_VERSION__ 198912L
 #endif
 
 #define STDC94 199409L
@@ -43,235 +43,237 @@
 #define STDCPP20 202002L
 
 #ifdef __cplusplus
-#define FOO_INLINE inline
+#    define FOO_INLINE inline
 #else
-#if __STDC_VERSION >= STDC99
-#define FOO_INLINE inline
-#endif
+#    if __STDC_VERSION >= STDC99
+#        define FOO_INLINE inline
+#    endif
 #endif
 
 #ifndef FOO_INLINE
-#ifdef _MSC_VER
-#define FOO_INLINE __inline
-#else
-#define FOO_INLINE __inline__
-#endif
+#    ifdef _MSC_VER
+#        define FOO_INLINE __inline
+#    else
+#        define FOO_INLINE __inline__
+#    endif
 #endif
 
 #if defined(__cplusplus)
-#if __cplusplus >= STDCPP11
-#define THREAD_LOCAL thread_local
-#endif
+#    if __cplusplus >= STDCPP11
+#        define THREAD_LOCAL thread_local
+#    endif
 #else
-#if __STDC_VERSION__ >= STDC11
-#if !defined(__STDC_NO_THREADS__) || !__STDC_NO_THREADS__
-#if __STDC_VERSION__ > STDC17
-#define THREAD_LOCAL thread_local
-#else
-#define THREAD_LOCAL _Thread_local
-#endif
-#endif
-#endif
+#    if __STDC_VERSION__ >= STDC11
+#        if !defined(__STDC_NO_THREADS__) || !__STDC_NO_THREADS__
+#            if __STDC_VERSION__ > STDC17
+#                define THREAD_LOCAL thread_local
+#            else
+#                define THREAD_LOCAL _Thread_local
+#            endif
+#        endif
+#    endif
 #endif
 
 #ifndef THREAD_LOCAL
-#ifdef _MSC_VER
-#define THREAD_LOCAL __declspec(thread)
-#else
-#define THREAD_LOCAL __thread
-#endif
+#    ifdef _MSC_VER
+#        define THREAD_LOCAL __declspec(thread)
+#    else
+#        define THREAD_LOCAL __thread
+#    endif
 #endif
 
 #if defined(__cplusplus)
-#if __cplusplus >= STDCPP11
-#define FOO_NORETURN [[noreturn]]
-#endif
+#    if __cplusplus >= STDCPP11
+#        define FOO_NORETURN [[noreturn]]
+#    endif
 #else
-#if __STDC_VERSION__ > STDC17
-#define FOO_NORETURN [[noreturn]]
-#elif __STDC_VERSION >= STDC11
-#define FOO_NORETURN _Noreturn
-#endif
+#    if __STDC_VERSION__ > STDC17
+#        define FOO_NORETURN [[noreturn]]
+#    elif __STDC_VERSION >= STDC11
+#        define FOO_NORETURN _Noreturn
+#    endif
 #endif
 
 #ifndef FOO_NORETURN
-#ifdef _MSC_VER
-#define FOO_NORETURN __declspec(noreturn)
-#else
-#define FOO_NORETURN __attribute__((noreturn))
-#endif
+#    ifdef _MSC_VER
+#        define FOO_NORETURN __declspec(noreturn)
+#    else
+#        define FOO_NORETURN __attribute__((noreturn))
+#    endif
 #endif
 
 #if defined(__cplusplus)
-#if __cplusplus >= STDCPP11
-#define ALIGNAS(n) alignas(n)
-#endif
+#    if __cplusplus >= STDCPP11
+#        define ALIGNAS(n) alignas(n)
+#    endif
 #else
-#if __STDC_VERSION__ > STDC17
-#define ALIGNAS(n) alignas(n)
-#elif __STDC_VERSION__ >= STDC11
-#define ALIGNAS(n) _Alignas(n)
-#endif
+#    if __STDC_VERSION__ > STDC17
+#        define ALIGNAS(n) alignas(n)
+#    elif __STDC_VERSION__ >= STDC11
+#        define ALIGNAS(n) _Alignas(n)
+#    endif
 #endif
 
 #ifndef ALIGNAS
-#ifdef _MSC_VER
-#define ALIGNAS(n) __declspec(align(n))
-#else
-#define ALIGNAS(n) __attribute__((aligned(n)))
-#endif
+#    ifdef _MSC_VER
+#        define ALIGNAS(n) __declspec(align(n))
+#    else
+#        define ALIGNAS(n) __attribute__((aligned(n)))
+#    endif
 #endif
 
 #if defined(__cplusplus)
-#if __cplusplus >= STDCPP11
-#define STATIC_ASSERT(n, ...)                                                  \
-    static_assert(n, UNIFORM_COMPILER_CONCAT_AS_STR(, __VA_ARGS__))
-#endif
+#    if __cplusplus >= STDCPP11
+#        define STATIC_ASSERT(n, ...)                                          \
+            static_assert(n, UNIFORM_COMPILER_CONCAT_AS_STR(, __VA_ARGS__))
+#    endif
 #else
-#if __STDC_VERSION__ > STDC17
-#define STATIC_ASSERT(n, ...) static_assert(n, #__VA_ARGS__)
-#elif __STDC_VERSION__ >= STDC11
-#define STATIC_ASSERT(n, ...) _Static_assert(n, #__VA_ARGS__)
-#endif
+#    if __STDC_VERSION__ > STDC17
+#        define STATIC_ASSERT(n, ...) static_assert(n, #__VA_ARGS__)
+#    elif __STDC_VERSION__ >= STDC11
+#        define STATIC_ASSERT(n, ...) _Static_assert(n, #__VA_ARGS__)
+#    endif
 #endif
 
 #ifndef STATIC_ASSERT
-#define _CSTATIC_ASSERT_CONCAT(msg, line, counter)                             \
-    static_assert_##line##_##counter##_failed_##msg
-#define CSTATIC_ASSERT_CONCAT(msg, line, counter)                              \
-    _CSTATIC_ASSERT_CONCAT(msg, line, counter)
-#define STATIC_ASSERT(n, ...)                                                  \
-    typedef char CSTATIC_ASSERT_CONCAT(__VA_ARGS__, __LINE__,                  \
-                                       __COUNTER__)[(n) ? 1 : -1]
+#    define _CSTATIC_ASSERT_CONCAT(msg, line, counter)                         \
+        static_assert_##line##_##counter##_failed_##msg
+#    define CSTATIC_ASSERT_CONCAT(msg, line, counter)                          \
+        _CSTATIC_ASSERT_CONCAT(msg, line, counter)
+#    define STATIC_ASSERT(n, ...)                                              \
+        typedef char CSTATIC_ASSERT_CONCAT(__VA_ARGS__, __LINE__,              \
+                                           __COUNTER__)[(n) ? 1 : -1]
 #endif
 
 #ifdef __cplusplus
-#if __cplusplus >= STDCPP14
-#define DEPRECATED(...)                                                        \
-    [[deprecated(UNIFORM_COMPILER_CONCAT_AS_STR(, __VA_ARGS__))]]
-#endif
+#    if __cplusplus >= STDCPP14
+#        define DEPRECATED(...)                                                \
+            [[deprecated(UNIFORM_COMPILER_CONCAT_AS_STR(, __VA_ARGS__))]]
+#    endif
 #else
-#if __STDC_VESRION__ > STDC17
-#define DEPRECATED(...)                                                        \
-    [[deprecated(UNIFORM_COMPILER_CONCAT_AS_STR(, __VA_ARGS__))]]
-#endif
+#    if __STDC_VESRION__ > STDC17
+#        define DEPRECATED(...)                                                \
+            [[deprecated(UNIFORM_COMPILER_CONCAT_AS_STR(, __VA_ARGS__))]]
+#    endif
 #endif
 
 #ifndef DEPRECATED
-#ifdef _MSC_VER
-#define DEPRECATED(...)                                                        \
-    __declspec(deprecated(UNIFORM_COMPILER_CONCAT_AS_STR(, __VA_ARGS__)))
-#else
-#define DEPRECATED(...)                                                        \
-    __attribute__((deprecated(UNIFORM_COMPILER_CONCAT_AS_STR(, __VA_ARGS__))))
-#endif
+#    ifdef _MSC_VER
+#        define DEPRECATED(...)                                                \
+            __declspec(                                                        \
+                deprecated(UNIFORM_COMPILER_CONCAT_AS_STR(, __VA_ARGS__)))
+#    else
+#        define DEPRECATED(...)                                                \
+            __attribute__((                                                    \
+                deprecated(UNIFORM_COMPILER_CONCAT_AS_STR(, __VA_ARGS__))))
+#    endif
 #endif
 
 #ifdef __cplusplus
-#if __cplusplus >= STDCPP17
-#define FALLTHROUGH [[fallthrough]]
-#endif
+#    if __cplusplus >= STDCPP17
+#        define FALLTHROUGH [[fallthrough]]
+#    endif
 #else
-#if __STDC_VESRION__ > STDC17
-#define FALLTHROUGH [[fallthrough]]
-#endif
+#    if __STDC_VESRION__ > STDC17
+#        define FALLTHROUGH [[fallthrough]]
+#    endif
 #endif
 
 #ifndef FALLTHROUGH
-#ifdef _MSC_VER
-#define FALLTHROUGH
-#else
-#define FALLTHROUGH __attribute__((fallthrough))
-#endif
+#    ifdef _MSC_VER
+#        define FALLTHROUGH
+#    else
+#        define FALLTHROUGH __attribute__((fallthrough))
+#    endif
 #endif
 
 #ifdef __cplusplus
-#if __cplusplus >= STDCPP17
-#define MAYBE_UNUSED [[maybe_unused]]
-#endif
+#    if __cplusplus >= STDCPP17
+#        define MAYBE_UNUSED [[maybe_unused]]
+#    endif
 #else
-#if __STDC_VESRION__ > STDC17
-#define MAYBE_UNUSED [[maybe_unused]]
-#endif
+#    if __STDC_VESRION__ > STDC17
+#        define MAYBE_UNUSED [[maybe_unused]]
+#    endif
 #endif
 
 #ifndef MAYBE_UNUSED
-#ifdef _MSC_VER
-#define MAYBE_UNUSED
-#else
-#define MAYBE_UNUSED __attribute__((unused))
-#endif
+#    ifdef _MSC_VER
+#        define MAYBE_UNUSED
+#    else
+#        define MAYBE_UNUSED __attribute__((unused))
+#    endif
 #endif
 
 #ifdef __cplusplus
-#if __cplusplus >= STDCPP20
-#define NODISCARD_RESULT(...)                                                  \
-    [[nodiscard(UNIFORM_COMPILER_CONCAT_AS_STR(, __VA_ARGS__))]]
-#elif __cplusplus >= STDCPP17
-#define NODISCARD_RESULT(...) [[nodiscard]]
-#endif
+#    if __cplusplus >= STDCPP20
+#        define NODISCARD_RESULT(...)                                          \
+            [[nodiscard(UNIFORM_COMPILER_CONCAT_AS_STR(, __VA_ARGS__))]]
+#    elif __cplusplus >= STDCPP17
+#        define NODISCARD_RESULT(...) [[nodiscard]]
+#    endif
 #else
-#if __STDC_VESRION__ > STDC17
-#define NODISCARD_RESULT(...)                                                  \
-    [[nodiscard(UNIFORM_COMPILER_CONCAT_AS_STR(, __VA_ARGS__))]]
-#endif
+#    if __STDC_VESRION__ > STDC17
+#        define NODISCARD_RESULT(...)                                          \
+            [[nodiscard(UNIFORM_COMPILER_CONCAT_AS_STR(, __VA_ARGS__))]]
+#    endif
 #endif
 
 #ifndef NODISCARD_RESULT
-#ifdef _MSC_VER
-#include <sal.h>
-#define NODISCARD_RESULT(...) _Check_return_
-#else
-#define NODISCARD_RESULT(...) __attribute__((warn_unused_result))
-#endif
+#    ifdef _MSC_VER
+#        include <sal.h>
+#        define NODISCARD_RESULT(...) _Check_return_
+#    else
+#        define NODISCARD_RESULT(...) __attribute__((warn_unused_result))
+#    endif
 #endif
 
 #ifdef __cplusplus
 #else
-#if __STDC_VESRION__ >= STDC99
-#define RESTRICT restrict
-#endif
+#    if __STDC_VESRION__ >= STDC99
+#        define RESTRICT restrict
+#    endif
 #endif
 
 #ifndef RESTRICT
-#ifdef _MSC_VER
-#define RESTRICT __restrict
-#else
-#define RESTRICT __restrict__
-#endif
+#    ifdef _MSC_VER
+#        define RESTRICT __restrict
+#    else
+#        define RESTRICT __restrict__
+#    endif
 #endif
 
 #ifdef __cplusplus
-#if __cplusplus >= STDCPP11
-#define NULLPTR nullptr
-#endif
+#    if __cplusplus >= STDCPP11
+#        define NULLPTR nullptr
+#    endif
 #else
-#if __STDC_VERSION__ > STDC17
-#define NULLPTR nullptr
-#endif
+#    if __STDC_VERSION__ > STDC17
+#        define NULLPTR nullptr
+#    endif
 #endif
 
 #ifndef NULLPTR
-#ifdef __cplusplus
-#include <cstddef>
-#define NULLPTR NULL
-#else
-#include <stddef.h>
-#define NULLPTR NULL
-#endif
+#    ifdef __cplusplus
+#        include <cstddef>
+#        define NULLPTR NULL
+#    else
+#        include <stddef.h>
+#        define NULLPTR NULL
+#    endif
 #endif
 
 #ifndef FORCE_FOO_INLINE
-#ifdef _MSC_VER
-#define FORCE_FOO_INLINE __forceinline
-#else
-#define FORCE_FOO_INLINE __attribute__((always_inline))
-#endif
+#    ifdef _MSC_VER
+#        define FORCE_FOO_INLINE __forceinline
+#    else
+#        define FORCE_FOO_INLINE __attribute__((always_inline))
+#    endif
 #endif
 
 #ifdef __GNUC__
-#undef GCC_VERSION
-#pragma pop_macro("GCC_VERSION")
+#    undef GCC_VERSION
+#    pragma pop_macro("GCC_VERSION")
 #endif
 
 #endif
